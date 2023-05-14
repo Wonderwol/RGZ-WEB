d20732dbe172e6e877800f961124f92e5b82c4a9
Темергалеев Никита <mr.wonderwol@mail.ru>
2023-05-15 06:35:58 +0700

    Готовая шапка и полурабочая функция для шапки

---
+++ rgzScript.js
@@ -0,0 +1,47 @@
+
+let teacherOffsetLeft = 0;
+let studentOffsetLeft = 750;
+let lecturerOffsetLeft = 1500;
+
+function slideNameLeft() {
+    let teacher = document.getElementById('teacher');
+    let student = document.getElementById('student');   
+    let lecturer = document.getElementById('lecturer');
+    
+    if (studentOffsetLeft == 750){
+        student.style.transform = 'translateX(-750px)';
+        lecturer.style.transform = 'translateX(-750px)';
+        studentOffsetLeft -= 750;
+        lecturerOffsetLeft -= 750;
+    }
+    
+    if (teacherOffsetLeft == 750) {
+        teacher.style.transform = 'translateX(-750px)';
+        student.style.transform = 'translateX(-750px)';
+        teacherOffsetLeft -= 750;
+        studentOffsetLeft -= 750;
+    }
+    return '${teacherOffsetLeft} ${studentOffsetLeft} ${lecturerOffsetLeft}';
+}
+
+function slideNameRight() {
+
+    let student = document.getElementById('student');
+    let teacher = document.getElementById('teacher');
+    let lecturer = document.getElementById('lecturer');
+
+    if (studentOffsetLeft == 750){
+        student.style.transform = 'translateX(750px)';
+        teacher.style.transform = 'translateX(750px)';
+        studentOffsetLeft += 750;
+        teacherOffsetLeft += 750;
+    }
+
+    if (lecturerOffsetLeft == 750) {
+        lecturer.style.transform = 'translateX(750px)';
+        student.style.transform = 'translateX(750px)';
+        lecturerOffsetLeft += 750;
+        studentOffsetLeft += 750;
+    }
+    return '${teacherOffsetLeft} ${studentOffsetLeft} ${lecturerOffsetLeft}';
+}
